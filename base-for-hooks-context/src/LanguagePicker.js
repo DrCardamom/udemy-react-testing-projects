fe64b557d68e5a5bcb6e45e1e562d3e501cfe820
DrCardamom <erkshimba@gmail.com>
2020-09-27 14:02:42 -0400

    Add test for proper rendering and props

---
+++ base-for-hooks-context/src/LanguagePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+import propTypes from "prop-types";
+
+function LanguagePicker({ setLanguage }) {
+   return (
+      <div data-test='component-language-picker'>
+
+      </div>
+   )
+}
+
+LanguagePicker.propTypes = {
+   setLanguage: propTypes.func.isRequired,
+}
+
+export default LanguagePicker;
